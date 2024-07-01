1f5c96d636a94e3bdde19bdc90b421056359cf3e
Gopi V <gopi@lp.local>
2024-07-01 21:44:52 +0530

    lease download wait time

--- src/main/java/mainPackage/PropertyWare.java
+++ src/main/java/mainPackage/PropertyWare.java
@@ -376,6 +376,7 @@ public class PropertyWare
 			 	filename = documents.get(i).getText();
 			 	RunnerClass.setFileName(filename);
 				checkLeaseAgreementAvailable = true;
+				Thread.sleep(40000);
 				PropertyWare.waitUntilFileIsDownloaded(filename);
 				break;
 			        }
